ef6cc37058b2ba9c2089f789c49693367908be1a
smskit726 <smskit726@gmail.com>
2020-05-21 17:27:55 +0900

    if 명령연습

---
+++ Java_04/src/com/biz/hello/Var_02.java
@@ -0,0 +1,67 @@
+package com.biz.hello;
+
+public class Var_02 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 8;
+		int num2 = 0;
+		
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		System.out.println(num1 + "X" + ++num2 + "=" + (num1*num2));
+		
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("=============================");
+		
+		// num2가 9가 되어 있는 상태이므로 ++num2를 실행하면 num2값은 10이 되어
+		// 이후에 출력되는 구구단은 8X10부터 출력이 되어 버린다.
+		num2=0; // 재설정, clear
+		
+		//num2에 어떠한 값이 있던지 상관없이 0으로 clear 되어 처음 시작하는 것과 같은 상태가 된다.
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		System.out.printf("%d x %d = %d", num1,++num2,(num1*num2));
+		System.out.println();
+		
+		System.out.println("=============================");
+		
+		num2=0;
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		System.out.printf("%d x %d = %d\n", num1,++num2,(num1*num2));
+		
+		
+		
+	}
+
+}
