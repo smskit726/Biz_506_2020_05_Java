0a45a6f3d0eee9f0635828321bf9a1573506126e
unknown <smskit726@gmail.com>
2020-09-04 14:57:37 +0900

    Interface

---
+++ Java_Interface/src/com/biz/interfaces/RemoConInterface.java
@@ -0,0 +1,13 @@
+package com.biz.interfaces;
+
+public interface RemoConInterface {
+
+	public void 전원ON();
+	public void 전원OFF();
+	public void 소리UP();
+	public void 소리DOWN();
+	
+	public void 채널UP();
+	public void 채널DOWN();
+	
+}
---
+++ Java_Interface/src/com/biz/service/나회사.java
@@ -0,0 +1,43 @@
+package com.biz.service;
+
+import com.biz.interfaces.RemoConInterface;
+
+public class 나회사 implements RemoConInterface{
+
+	@Override
+	public void 전원ON() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void 전원OFF() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void 소리UP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void 소리DOWN() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void 채널UP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void 채널DOWN() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
